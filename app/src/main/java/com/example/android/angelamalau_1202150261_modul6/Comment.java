65143ba0a58dc51ee101d8924bd25c75d9d73d47
angelamalau <36089525+angelamalau@users.noreply.github.com>
2018-04-01 23:00:20 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/android/angelamalau_1202150261_modul6/Comment.java
@@ -0,0 +1,28 @@
+package com.example.android.angelamalau_1202150261_modul6;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+
+@IgnoreExtraProperties
+public class Comment {
+    public String uid;
+    public String author;
+    public String text;
+
+    public Comment() {
+        // Default constructor required for calls to DataSnapshot.getValue(Comment.class)
+
+    }
+
+
+
+    public Comment(String uid, String author, String text) {
+
+        this.uid = uid;
+
+        this.author = author;
+
+        this.text = text;
+
+    }
+}
